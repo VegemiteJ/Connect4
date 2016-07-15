5d8cb759a3de08a376d167f61c781d156e1b322a
VegemiteJ <vegemitewarrior@gmail.com>
2016-07-14 20:21:09 -0400

    canges

---
+++ tests/NodeTests/nodeT.cpp
@@ -0,0 +1,13 @@
+#include "Node.h"
+#include "consts.h"
+#include "ColourDef.h"
+#include <iostream>
+
+int verbose = 4;
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "HELLO!" << endl;	
+}
